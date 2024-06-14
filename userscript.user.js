bc2ac04c0bfeb0fcb2da4767d3ae84d1fcb00ebe
Stanley Wang <stanley2058@yahoo.com.tw>
2024-06-14 17:58:54 +0800

    feat: v1.17 canvas sponsor detector

--- userscript.user.js
+++ userscript.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Remove Facebook Ad Posts
-// @version      1.16
+// @version      1.17
 // @author       STW
 // @match        https://www.facebook.com/*
 // @require      https://unpkg.com/@reactivex/rxjs/dist/global/rxjs.umd.min.js
@@ -14,17 +14,24 @@
 // GitHub Repo: https://github.com/stanley2058/RemoveFacebookAds-ZH
 // Direct Link: https://github.com/stanley2058/RemoveFacebookAds-ZH/raw/main/userscript.user.js
 // Change the threshold to match your desire, -1 will remove all ads.
+
+unsafeWindow.AD_Version = "1.17";
+
 const threshold = 10000;
 const lookBack = 15;
 
 const sponsorIdentifiers = ["贊", "助"];
+const canvasSponsorIdentifiers = "贊助";
+const canvasFontSize = 34;
+const canvasSimilarityThreshold = 0.005;
 const commentIdentifiers = {
-    comment: "留言",
-    mostRelated: "最相關",
-    allComment: "所有留言"
+  comment: "留言",
+  mostRelated: "最相關",
+  allComment: "所有留言",
 };
 
 /* Change Log
+1.17   - Implement canvas sponsor text detection.
 1.16   - Update post selector. Remove delete button.
 1.15   - Force all comment is back now!
 1.14.4 - Fix feed selector.
@@ -54,8 +61,6 @@ const commentIdentifiers = {
 const { fromEvent, interval } = rxjs;
 const { throttleTime } = rxjs.operators;
 
-unsafeWindow.AD_Version = "1.16";
-
 unsafeWindow.deletedPost = [];
 unsafeWindow.deletedPostOwner = [];
 
@@ -76,23 +81,37 @@ const deleteAd = () => {
   const sponsorDiv = document.querySelector("#ssrb_rhc_start + div span");
   if (sponsorDiv) sponsorDiv.innerHTML = "";
 
-  const feed = Array.from(document.querySelectorAll("div:not(.html-div):not([class]):has(> div.html-div)"));
+  const feed = Array.from(
+    document.querySelectorAll(
+      "div:not(.html-div):not([class]):has(> div.html-div)",
+    ),
+  );
   if (feed.length === 0) return;
   const start = feed.length > lookBack ? feed.length - lookBack : 0;
   feed.slice(start, feed.length).forEach((div) => {
+    // canvas method
+    const canvas = div.querySelector("canvas");
+    const canvasSponsor =
+      (canvas && canvasSimilar(createEqCanvas(canvas), canvas)) || false;
+
+    // svg method
     const useLayer = div.querySelector("svg use");
-    if (!useLayer) return;
-    const texts = document.querySelector(useLayer.getAttribute("xlink:href") || "")?.innerHTML || "";
+    let isSvgSponsor = false;
+    if (useLayer) {
+      const texts =
+        document.querySelector(useLayer.getAttribute("xlink:href") || "")
+          ?.innerHTML || "";
+      const set = new Set(texts);
+      isSvgSponsor = sponsorIdentifiers.every((i) => set.has(i));
+    }
 
-    const set = new Set(texts);
-    const isSponsor = sponsorIdentifiers.every(i => set.has(i));
-
+    const isSponsor = isSvgSponsor || canvasSponsor;
     if (!isSponsor) return;
 
     const socialNum = Math.max(
       ...[...div.querySelectorAll("div[role='button'] > span")]
         .map((e) => parseInt(e.innerText))
-        .filter((n) => isFinite(n))
+        .filter((n) => isFinite(n)),
     );
     const name = div.querySelector("h4 a").innerText;
     if (
@@ -114,40 +133,81 @@ const deleteAd = () => {
 };
 
 const forceAllComment = () => {
-    const dropMenu = Array.from(document.querySelectorAll('h3[dir="auto"],h2[dir="auto"]'))
-        .filter(h => h.innerText === commentIdentifiers.comment)
-        .flatMap(h => Array.from(h.parentElement.querySelectorAll('div[role="button"]')))
-            .find(e => e.innerText.includes(commentIdentifiers.mostRelated));
-    if (!dropMenu) return;
-    dropMenu.click();
-    setTimeout(() => {
-        Array.from(document.querySelectorAll('div[role="menuitem"]'))
-            .find(e => e.innerText.startsWith(commentIdentifiers.allComment))?.click();
-    }, 1)
+  const dropMenu = Array.from(
+    document.querySelectorAll('h3[dir="auto"],h2[dir="auto"]'),
+  )
+    .filter((h) => h.innerText === commentIdentifiers.comment)
+    .flatMap((h) =>
+      Array.from(h.parentElement.querySelectorAll('div[role="button"]')),
+    )
+    .find((e) => e.innerText.includes(commentIdentifiers.mostRelated));
+  if (!dropMenu) return;
+  dropMenu.click();
+  setTimeout(() => {
+    Array.from(document.querySelectorAll('div[role="menuitem"]'))
+      .find((e) => e.innerText.startsWith(commentIdentifiers.allComment))
+      ?.click();
+  }, 1);
+};
+
+function createEqCanvas(
+  c,
+  text = canvasSponsorIdentifiers,
+  fontSize = canvasFontSize,
+) {
+  const ctxCmp = c.getContext("2d");
+  const canvas = document.createElement("canvas");
+  canvas.width = c.width;
+  canvas.height = c.height;
+  const ctx = canvas.getContext("2d");
+  ctx.font = `${fontSize}px Helvetica, Arial, sans-serif`;
+  ctx.fillStyle = ctxCmp.fillStyle;
+  ctx.textBaseline = ctxCmp.textBaseline;
+  ctx.fillText(text, 0, canvasFontSize / 2 + 1);
+  return canvas;
 }
 
-fromEvent(window, "scroll")
-  .pipe(throttleTime(300))
-  .subscribe(deleteAd);
+function canvasSimilar(
+  canvas1,
+  canvas2,
+  threshold = canvasSimilarityThreshold,
+) {
+  const ctx1 = canvas1.getContext("2d");
+  const ctx2 = canvas2.getContext("2d");
+
+  const data1 = ctx1.getImageData(0, 0, canvas1.width, canvas1.height).data;
+  const data2 = ctx2.getImageData(0, 0, canvas2.width, canvas2.height).data;
+
+  if (data1.length !== data2.length) return false;
+  let mismatch = 0;
+  for (let i = 0; i < data1.length; i++) {
+    if (data1[i] !== data2[i] && (!data1[i] || !data2[i])) {
+      mismatch++;
+    }
+  }
+  return mismatch < data1.length * threshold;
+}
+
+fromEvent(window, "scroll").pipe(throttleTime(300)).subscribe(deleteAd);
 
 interval(200).subscribe(forceAllComment);
 
 unsafeWindow.AD_Help = () => {
   console.log(
     "%cFacebook AD Post Blocker",
-    "color: yellow; background-color: navy; font-size: 24pt;"
+    "color: yellow; background-color: navy; font-size: 24pt;",
   );
   console.log(
     `%cCurrent Version: ${AD_Version}`,
-    "color: cyan; background-color: navy; font-size: 12pt;"
+    "color: cyan; background-color: navy; font-size: 12pt;",
   );
   console.log(
     "%cCheck Update: https://github.com/stanley2058/RemoveFacebookAds-ZH/raw/main/userscript.user.js",
-    "color: cyan; background-color: navy; font-size: 12pt;"
+    "color: cyan; background-color: navy; font-size: 12pt;",
   );
   console.log(
     "%cOptions:",
-    "color: greenyellow; background-color: navy; font-size: 18pt;"
+    "color: greenyellow; background-color: navy; font-size: 18pt;",
   );
   console.table([
     {
@@ -166,7 +226,7 @@ unsafeWindow.AD_Help = () => {
   ]);
   console.log(
     "%cCurrent Blocked:",
-    "color: greenyellow; background-color: navy; font-size: 16pt;"
+    "color: greenyellow; background-color: navy; font-size: 16pt;",
   );
   console.log(blockList);
 };
@@ -174,7 +234,7 @@ unsafeWindow.AD_Help = () => {
 unsafeWindow.AD_ShowDeletedPosts = () => {
   for (let i = 0; i < unsafeWindow.deletedPostOwner.length; ++i) {
     console.log(
-      `${i}: ${unsafeWindow.deletedPostOwner[i].name}\n${unsafeWindow.deletedPostOwner[i].url}`
+      `${i}: ${unsafeWindow.deletedPostOwner[i].name}\n${unsafeWindow.deletedPostOwner[i].url}`,
     );
   }
 };
